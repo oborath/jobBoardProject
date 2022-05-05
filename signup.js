4e04c0f4482f450295973011e7d2207901b6f408
Vibolrottana Seng <rattana90@yahoo.com>
2022-05-05 22:36:49 +0700

    3 files updated

---
+++ signup.js
@@ -0,0 +1,11 @@
+
+
+var firebaseConfig = {
+    apiKey: "api-key",
+    authDomain: "project-id.firebaseapp.com",
+    databaseURL: "https://project-id.firebaseio.com",
+    projectId: "project-id",
+    storageBucket: "project-id.appspot.com",
+    messagingSenderId: "sender-id",
+};
+
