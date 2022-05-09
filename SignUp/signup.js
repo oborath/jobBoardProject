3563e9662ddc7793a0d84ba37baad737e29d35a4
Vibolrottana Seng <rattana90@yahoo.com>
2022-05-09 22:08:07 +0700

    Update signup.js

--- SignUp/signup.js
+++ SignUp/signup.js
@@ -1,37 +1,14 @@
 
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.0/firebase-app.js";
-// import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.8.0/firebase-analytics.js";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
+  // Import the functions you need from the SDKs you need
+    import {initializeApp} from "https://www.gstatic.com/firebasejs/9.8.0/firebase-app.js";
+    import { getDatabase, ref, set, get, child } from "https://www.gstatic.com/firebasejs/9.8.0/firebase-app.js";
+    // TODO: Add SDKs for Firebase products that you want to use
+    // https://firebase.google.com/docs/web/setup#available-libraries
 
-// // Your web app's Firebase configuration
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// var config = {
-//     apiKey: "AIzaSyDgkxfJZsCv0OYojrDzjmY8YzjZWLRjAlg",
-//     authDomain: "job-board-55b1b.firebaseapp.com",
-//     databaseURL: "https://job-board-55b1b-default-rtdb.firebaseio.com",
-//     projectId: "job-board-55b1b",
-//     storageBucket: "job-board-55b1b.appspot.com",
-//     messagingSenderId: "830385285782",
-//     appId: "1:830385285782:web:4d5e52ebcc77343836f0b7",
-//     measurementId: "G-JR7EZSBGHS"
-// };
-
-// // Initialize Firebase
-// firebase.initializeApp(config);
-
-
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.0/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.8.0/firebase-analytics.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const config = {
-    apiKey: "AIzaSyDrwtklTOMhsRRDaWo34o_saDizghVkRQ4",
+    // Your web app's Firebase configuration
+    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+    const firebaseConfig = {
+        apiKey: "AIzaSyDrwtklTOMhsRRDaWo34o_saDizghVkRQ4",
     authDomain: "job-board-2beab.firebaseapp.com",
     databaseURL: "https://job-board-2beab-default-rtdb.firebaseio.com",
     projectId: "job-board-2beab",
@@ -39,14 +16,31 @@ const config = {
     messagingSenderId: "631501036101",
     appId: "1:631501036101:web:aa68add3b603d4de3c212c",
     measurementId: "G-1C0Y8M7GL1"
-};
+  };
 
-// Initialize Firebase
-firebase.initializeApp(config);
+    // Initialize Firebase
+    
+
+    const app = initializeApp(firebaseConfig);
+    const db = getDatabase(app);
+
+    document.getElementById('form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    set(ref(db, 'users/' + Math.random().toString(36).slice(2, 7)), {
+        firstName: document.getElementById('firstName').value,
+        email: document.getElementById('lastName').value,
+        subject: document.getElementById('subject').value,
+        message: document.getElementById('message').value
+    });
+ 
+    alert('Your form is submitted');
+    document.getElementById('frmContact').reset();
+});
 
 
 
-var con = firebase.database().ref('users');
+
+
 
 document.getElementById("form").addEventListener("submit", (e) => {
     e.preventDefault();
