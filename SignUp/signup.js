d7d00bdb19c10c7709e373e51c07f83c65b94249
Effy Borath <80004367+e-githubUser@users.noreply.github.com>
2022-05-12 09:01:19 -0700

    JKKNJ

--- SignUp/signup.js
+++ SignUp/signup.js
@@ -1,63 +1,57 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later,
+// measurementId is optional
 
-//   // Import the functions you need from the SDKs you need
-//     import {initializeApp} from "https://www.gstatic.com/firebasejs/9.8.0/firebase-app.js";
-//     import { getDatabase, ref, set, get, child } from "https://www.gstatic.com/firebasejs/9.8.0/firebase-app.js";
-//     // TODO: Add SDKs for Firebase products that you want to use
-//     // https://firebase.google.com/docs/web/setup#available-libraries
+const firebaseConfig = {
+    apiKey: "AIzaSyDgkxfJZsCv0OYojrDzjmY8YzjZWLRjAlg",
+    authDomain: "job-board-55b1b.firebaseapp.com",
+    databaseURL: "https://job-board-55b1b-default-rtdb.firebaseio.com",
+    projectId: "job-board-55b1b",
+    storageBucket: "job-board-55b1b.appspot.com",
+    messagingSenderId: "830385285782",
+    appId: "1:830385285782:web:4d5e52ebcc77343836f0b7",
+    measurementId: "G-JR7EZSBGHS"
+};
 
-//     // Your web app's Firebase configuration
-//     // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-//     const firebaseConfig = {
-//     apiKey: "AIzaSyDrwtklTOMhsRRDaWo34o_saDizghVkRQ4",
-//     authDomain: "job-board-2beab.firebaseapp.com",
-//     databaseURL: "https://job-board-2beab-default-rtdb.firebaseio.com",
-//     projectId: "job-board-2beab",
-//     storageBucket: "job-board-2beab.appspot.com",
-//     messagingSenderId: "631501036101",
-//     appId: "1:631501036101:web:aa68add3b603d4de3c212c",
-//     measurementId: "G-1C0Y8M7GL1"
-//   };
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-//     // Initialize Firebase
-    
+const auth = firebase.auth();
 
-//     const app = initializeApp(firebaseConfig);
-//     const db = getDatabase(app);
+// Signup function
+function signUp() {
+var email = document.getElementById("email");
+var password = document.getElementById("password");
 
-//     document.getElementById('form').addEventListener('submit', function(e) {
-//     e.preventDefault();
-//     set(ref(db, 'users/' + Math.random().toString(36).slice(2, 7)), {
-//         firstName: document.getElementById('firstName').value,
-//         email: document.getElementById('lastName').value,
-//         subject: document.getElementById('subject').value,
-//         message: document.getElementById('message').value
-//     });
- 
-//     alert('Your form is submitted');
-//     document.getElementById('frmContact').reset();
-// });
+const promise = auth.createUserWithEmailAndPassword(
+    email.value,
+    password.value
+);
+promise.catch((e) => alert(e.message));
+alert("SignUp Successfully");
+}
 
+// SignIN function
+function signIn() {
+var email = document.getElementById("email");
+var password = document.getElementById("password");
+const promise = auth.signInWithEmailAndPassword(
+            email.value, password.value);
+promise.catch((e) => alert(e.message));
+}
 
-// document.getElementById("form").addEventListener("submit", (e) => {
-//     e.preventDefault();
+// SignOut
+function signOut() {
+auth.signOut();
+alert("SignOut Successfully from System");
+}
 
-
-//     var userInfo = con.push();
-//     userInfo.set({
-//         firstname: getId("firstName"),
-//         lastname: getId("lastName"),
-//         studentID: getId("studentid"),
-//         DateofBirth: getId("dob"),
-//         Gender: getId("gender"),
-//         email: getId("email"),
-//         password: getId("password")
-
-//     });
-//     alert("sent");
-//     console.log("sent")
-//     document.getElementById("form").reset();
-// });
-
-// function getId(id) {
-//     return document.getElementById(id).value;
-// }
+// Active user to homepage
+firebase.auth().onAuthStateChanged((user) => {
+if (user) {
+    var email = user.email;
+    alert("Active user " + email);
+} else {
+    alert("No Active user Found");
+}
+});
