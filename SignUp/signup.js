cd1cb493d8e4cfcd407bf5d0398ba8796d91edd7
Effy Borath <80004367+e-githubUser@users.noreply.github.com>
2022-05-15 07:55:54 -0700

    updated javascript for sign up

--- SignUp/signup.js
+++ SignUp/signup.js
@@ -1,57 +1,55 @@
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later,
-// measurementId is optional
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later,
+// // measurementId is optional
+// const firebaseConfig = {
+//     apiKey: "AIzaSyDrwtklTOMhsRRDaWo34o_saDizghVkRQ4",
+//     authDomain: "job-board-2beab.firebaseapp.com",
+//     databaseURL: "https://job-board-2beab-default-rtdb.firebaseio.com",
+//     projectId: "job-board-2beab",
+//     storageBucket: "job-board-2beab.appspot.com",
+//     messagingSenderId: "631501036101",
+//     appId: "1:631501036101:web:aa68add3b603d4de3c212c",
+//     measurementId: "G-1C0Y8M7GL1"
+//   };
 
-const firebaseConfig = {
-    apiKey: "AIzaSyDgkxfJZsCv0OYojrDzjmY8YzjZWLRjAlg",
-    authDomain: "job-board-55b1b.firebaseapp.com",
-    databaseURL: "https://job-board-55b1b-default-rtdb.firebaseio.com",
-    projectId: "job-board-55b1b",
-    storageBucket: "job-board-55b1b.appspot.com",
-    messagingSenderId: "830385285782",
-    appId: "1:830385285782:web:4d5e52ebcc77343836f0b7",
-    measurementId: "G-JR7EZSBGHS"
-};
+//   firebase.initializeApp(firebaseConfig);
 
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+//   var messagesRef = firebase.database()
+//     .ref('Collected Data');
+  
+//   document.getElementById('form')
+//     .addEventListener('submit', submitForm);
 
-const auth = firebase.auth();
+//   function submitForm(e) {
+//     e.preventDefault();
 
-// Signup function
-function signUp() {
-var email = document.getElementById("email");
-var password = document.getElementById("password");
+//     // Get values
+//     var firstName = getInputVal('firstName');
+//     var lastName = getInputVal('lastName');
+//     var email = getInputVal('email');
+//     var studentid = getInputVal('studentid');
+//     var dob = getInputVal('dob');
+//     var gender = getInputVal('gender');
+//     var password = getInputVal('password');
+//     saveMessage(firstName, lastName, email, studentid, dob, gender, password);
+//     document.getElementById('form').reset();
+//   }
 
-const promise = auth.createUserWithEmailAndPassword(
-    email.value,
-    password.value
-);
-promise.catch((e) => alert(e.message));
-alert("SignUp Successfully");
-}
+//   // Function to get get form values
+//   function getInputVal(id) {
+//     return document.getElementById(id).value;
+//   }
 
-// SignIN function
-function signIn() {
-var email = document.getElementById("email");
-var password = document.getElementById("password");
-const promise = auth.signInWithEmailAndPassword(
-            email.value, password.value);
-promise.catch((e) => alert(e.message));
-}
-
-// SignOut
-function signOut() {
-auth.signOut();
-alert("SignOut Successfully from System");
-}
-
-// Active user to homepage
-firebase.auth().onAuthStateChanged((user) => {
-if (user) {
-    var email = user.email;
-    alert("Active user " + email);
-} else {
-    alert("No Active user Found");
-}
-});
+//   // Save message to firebase
+//   function saveMessage(firstName, lastName, email, studentid, dob, gender, password) {
+//     var newMessageRef = messagesRef.push();
+//     newMessageRef.set({
+//       firstName: firstName,
+//       lastName: lastName,
+//       email: email,
+//       studentid: studentid,
+//       dob: dob,
+//       gender: gender,
+//       password: password,
+//     });
+//   }
--- form.js
+++ form.js
@@ -3,17 +3,16 @@
 // measurementId is optional
 
 const firebaseConfig = {
-        apiKey: "AIzaSyDrwtklTOMhsRRDaWo34o_saDizghVkRQ4",
-        authDomain: "job-board-2beab.firebaseapp.com",
-        databaseURL: "https://job-board-2beab-default-rtdb.firebaseio.com",
-        projectId: "job-board-2beab",
-        storageBucket: "job-board-2beab.appspot.com",
-        messagingSenderId: "631501036101",
-        appId: "1:631501036101:web:aa68add3b603d4de3c212c",
-        measurementId: "G-1C0Y8M7GL1"
+    apiKey: "AIzaSyDrwtklTOMhsRRDaWo34o_saDizghVkRQ4",
+    authDomain: "job-board-2beab.firebaseapp.com",
+    databaseURL: "https://job-board-2beab-default-rtdb.firebaseio.com",
+    projectId: "job-board-2beab",
+    storageBucket: "job-board-2beab.appspot.com",
+    messagingSenderId: "631501036101",
+    appId: "1:631501036101:web:aa68add3b603d4de3c212c",
+    measurementId: "G-1C0Y8M7GL1"
 };
-      
-    
+
     // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
     
